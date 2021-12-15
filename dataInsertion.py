17beedfa54042552ea7c74b536ea4f60e38b5ba5
igumiao <96001989+igumiao@users.noreply.github.com>
2021-12-15 22:43:53 +0800

    only stadium and club work
    其他的还得慢慢改 
    记得加  ‘’

--- dataInsertion.py
+++ dataInsertion.py
@@ -9,6 +9,7 @@ Created on Sat Dec 11 10:43:14 2021
 #import libraries
 import pandas as pd
 import sqlite3 as s
+import codecs
 from sqlite3 import Error
 
 def createConnection(path):
@@ -20,10 +21,29 @@ def createConnection(path):
         
     return connection
 
+#网上照抄的execute query sqlite 本身没有execute query这个function 但有其他的
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("it works")
+    except Error as e:
+        print(f"The error '{e}' occurred")
 
+#insert的顺序要注意 因为我们现在有commit了
+def main():
+    dbName="Football.db"
+    Connection=createConnection(dbName)
+    fileName1="ClubStadium.csv"
+    insertClub(Connection, readTable(fileName1))
+    insertStadium(Connection, readTable(fileName1))
+    
+
+    
 def readTable(path):
-    dataframe = pd.read_csv(path)
-   
+    #这里 gb2312是我网上找的 因为我们的csv不是utf-8 能应对utf-8 encoding 报错 目前还没有问题 
+    dataframe = pd.read_csv(path,encoding = 'gb2312')
     return dataframe
 
 
@@ -31,47 +51,48 @@ def insertStadium(connection, df):
     
     for row in range(df.shape[0]):
         
-        insertion = "INSERT INTO stadium (sName, ctName, capacity, Year of Built) VALUES ({}, {}, {}, {})".format(df.iloc[row]['sName'],df.iloc[row]['ctName'],df.iloc[row]['capacity'], df.iloc[row]['yearOfBuilt'])
-        s.execute_query(connection, insertion)
+        insertion = "INSERT INTO stadium (sName, ctName, capacity,yearOfBuilt) VALUES ('{}', '{}', {}, {});".format(df.iloc[row]['sName'],df.iloc[row]['ctName'],df.iloc[row]['capacity'], df.iloc[row]['yearOfBuilt'])
+        execute_query(connection, insertion)
 
 
 def insertClub(connection, df):
     
     for row in range(df.shape[0]):
-        insertion = "INSERT INTO club (cName, nickName, foundYear, ctName, officialSite) VALUES ({}, {}, {}, {}, {})".format(df.iloc[row]['cName'],df.iloc[row]['nickname'],df.iloc[row]['foundYear'], df.iloc[row]['ctName'], df.iloc[row]['officialSite'])
-        s.execute_query(connection, insertion)
-###以下需要改对应csv colum的名字
+        insertion = "INSERT INTO club (cName, nickName, foundYear, ctName, officialSite) VALUES ('{}', '{}', {}, '{}', '{}');".format(df.iloc[row]['host'],df.iloc[row]['nickName'],df.iloc[row]['foundYear'], df.iloc[row]['ctName'], df.iloc[row]['officialSite'])
+        print(insertion)
+        execute_query(connection, insertion)
+
 def insertAttend(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO attend (ID, matchID) VALUES ({}, {})".format(df.iloc[row]['AAA'],df.iloc[row]['BBB'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 def insertBelong(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO belong (ID,ctName ) VALUES ({}, {})".format(df.iloc[row]['AAA'],df.iloc[row]['BBB'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 def insertCountry(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO country (ctName) VALUES ({})".format(df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 
 def insertEvent(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO event (eName,season,cName) VALUES ({},{},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 
 def insertFriendly(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO friendly (eName) VALUES ({})".format(df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
     
 
 
@@ -79,21 +100,21 @@ def insertHomeCourt(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO homeCourt (sName,cName) VALUES ({},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
     
 
 def insertHostStadium(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO hostStadium (host,sName) VALUES ({},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 
 def insertLeague(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO league (eName,ctName,size) VALUES ({},{},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
     
 
 
@@ -101,43 +122,46 @@ def insertMatch(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO match (eName,ctName,size) VALUES ({},{},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 
 def insertMatchTable(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO matchTable (host,visit,matchDate,eName,awayShots,homeShots,awayBooks,homeBooks) VALUES ({},{},{},{},{},{},{},{}".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'],)
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 
 def insertParticipate(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO participate (cName,eName) VALUES ({},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 def insertPeople(connection, df, type):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO people (ID) VALUES ({})".format(df.iloc[row][type])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 def insertPlayer(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO player (ID,cName,pName,DOB) VALUES ({},{},{},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 def insertRounds(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO rounds (size,numberOfRounds) VALUES ({},{})".format(df.iloc[row]['AAA'],df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
 
 
 def insertTournamnet(connection, df):
     
     for row in range(df.shape[0]):
         insertion = "INSERT INTO tournament (eName) VALUES ({})".format(df.iloc[row]['AAA'])
-        s.execute_query(connection, insertion)
+        execute_query(connection, insertion)
+    
+if __name__ == "__main__":
+    main()
