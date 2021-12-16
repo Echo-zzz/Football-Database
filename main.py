9b2d0d84205e0d843ca44a2e8e6c790e3eb2e893
RoarOrSmile <77243439+RoarOrSmile@users.noreply.github.com>
2021-12-16 10:13:42 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+import sqlite3
+from tkinter import *
+from tkinter.ttk import *
+import table as ta
+
+
+class main(Frame):
+    def __init__(self, master1=None):
+        Frame.__init__(self, master1)
+
+        self.master.title('FootBall')
+        self.master.geometry('1000x1000')
+
+        self.connect()
+        self.eventList()
+        self.show()
+        self.SELECT()
+
+    def connect(self):
+        global DB
+        try:
+            self.DB = sqlite3.connect("Football.db")
+            self.cursor = self.DB.cursor()
+            print("success!")
+        except:
+            print("warning, connect fail!")
+
+    def eventList(self):
+        self.stringvar = StringVar()
+        self.pull = Combobox(self.master, text=self.stringvar, state='readonly')
+        self.stringvar = ["Teams participating in both the Champions League and the League", "All winners to win "
+                                                                                             "derbies"]
+        self.pull["value"] = [i for i in self.stringvar]
+        self.pull.current(0)
+        self.pull.place(relx=0.3, rely=0.2, relwidth=0.45, relheight=0.047)
+
+    def SELECT(self):
+        self.button2 = Button(self.master, text='Go', command=self.reply)
+        self.button2.place(relx=0.4, rely=0.4, relwidth=0.214, relheight=0.095)
+
+    def reply(self):
+        if self.pull.get() == self.stringvar[0]:
+            self.eventPage(0)
+        if self.pull.get() == self.stringvar[1]:
+            self.eventPage(1)
+
+    def eventPage(self, inx):
+        self.top = Tk()
+        self.top.title("output")
+        self.event(inx)
+        self.top.geometry('1000x500')
+
+    def event(self, index):
+
+        global SQL
+        if index == 0:
+            print("1")
+            SQL = "SELECT * from league"
+        if index == 1:
+            print("2")
+            SQL = "SELECT * from event"
+
+        data = self.cursor.execute(SQL)
+
+        column = [column[0] for column in self.cursor.description]  # return the heading name
+
+        self.list = Treeview(self.top)
+
+        self.list["columns"] = column
+
+        for i in column:
+            self.list.column(i, anchor="center")
+
+        for i in column:
+            self.list.heading(i, text=i)
+        count = 1
+        for row in data:
+            print(row)
+            self.list.insert("", count, text="{}".format(count), values=row)
+            count = count + 1
+        self.list.pack()
+
+    def show(self):
+        self.button = Button(self.master, text='Show all table details', command=self.showBottom)
+        self.button.place(relx=0.6, rely=0.8, relwidth=0.2, relheight=0.05)
+
+    def showBottom(self):
+        self.master.destroy()
+        ta.Club().__init__()
+
+
+if __name__ == '__main__':
+    main_window = main()
+    main_window.mainloop()
---
+++ table.py
@@ -0,0 +1,85 @@
+import sqlite3
+from tkinter import *
+from tkinter.ttk import *
+import main as main
+
+class Club(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master.title('main')
+        self.master.geometry('500x500')
+        self.connect()
+        self.whichTable()
+        self.SELECT()
+        self.back()
+
+    def back(self):
+        self.button2 = Button(self.master, text='back', command=self.backBottom)
+        self.button2.place(relx=0.6, rely=0.8, relwidth=0.2, relheight=0.05)
+
+    def backBottom(self):
+        self.master.destroy()
+        main.main().__init__()
+
+    def whichTable(self):
+        self.stringvar = StringVar()
+        self.pull = Combobox(self.master, text=self.stringvar, state='readonly')
+        self.cursor.execute("SELECT name FROM sqlite_master where type='table' order by name;")
+        tableName = self.cursor.fetchall()
+        print(tableName)
+        self.pull["value"] = tableName
+        self.pull.current(0)
+        self.pull.place(relx=0.4, rely=0.2, relwidth=0.214, relheight=0.055)
+
+    def reply(self):
+        self.output()
+
+    def SELECT(self):
+
+        self.button = Button(self.master, text='go ahead', command=self.reply)
+        self.button.place(relx=0.4, rely=0.4, relwidth=0.214, relheight=0.095)
+
+    def output(self):
+        self.top = Tk()
+        self.top.title("output")
+        self.top.geometry('1000x500')
+        self.list = Treeview(self.top)
+        self.showAll()
+        self.list.pack()
+        self.INSERT()
+
+    def connect(self):
+        global DB
+        try:
+            self.DB = sqlite3.connect("Football.db")
+            self.cursor = self.DB.cursor()
+            print("success!")
+        except:
+            print("warning, connect fail!")
+
+    def INSERT(self):
+
+        self.enter = Entry(self.top)
+        self.enter.place(relx=0.4, rely=0.7, relwidth=0.214, relheight=0.095)
+
+    def insert(self, cName, ctName, nickName, foundYear, officialSite):
+        sql = "INSERT INTO '%s' (cName, ctName, NickName, foundYear, officialSite) VALUE (?,?,?,?);" % self.pull.get()
+        self.cursor.execute(sql)
+
+    def showAll(self):
+        sql = "SELECT * from '%s'" % self.pull.get()
+        self.cursor.execute(sql)
+        data = self.cursor.fetchall()
+        column = [column[0] for column in self.cursor.description]  # return the heading name
+        print(column)
+        self.list["columns"] = column
+        for i in column:
+            self.list.column(i, anchor="center")
+
+        for i in column:
+            self.list.heading(i, text=i)
+        count = 1
+        for row in data:
+            print(row)
+            self.list.insert("", count, text="{}".format(count), values=row)
+            count = count + 1
